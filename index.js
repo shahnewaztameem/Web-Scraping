797c99fe13ee070d3078029cc9d3a6037c29afea
shahnewaz tameem <shahnewaztamim@gmail.com>
2020-07-25 22:18:51 +0600

    Add axios, cheerio to load and get data

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+/* // setup dot env env
 require('dotenv').config();
-const debug = require('debug')('app');
+
+const debug = require('debug')('app:api key');
+
+// read API_KEY from dot env
 debug(process.env.API_KEY);
+ */
+
+const debug = require('debug')('app:axios');
+const axois = require('axios');
+const cheerio = require('cheerio');
+
+async function main() {
+  const res = await axois('http://bikroy.com');
+  const data = await res.data;
+  const $ = cheerio.load(data); 
+  debug($('h1').text())
+}
+main()
